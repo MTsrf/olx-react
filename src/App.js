7f8985ecd03983dcca2741991aff8bf594a5a178
HP <hp@pop-os.localdomain>
2022-08-03 16:30:07 +0530

    everything add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useContext } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import Create from './Pages/Create'
+import Post from './Pages/ViewPost'
+import { AuthContext, FirebaseContext } from './store/firebaseContext'
+import Poster from './store/PostContext'
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
 
 function App() {
+  const { setUser } = useContext(AuthContext)
+  const { auth } = useContext(FirebaseContext)
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user)
+    });
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Poster>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/post' element={<Post />} />
+        </Routes>
+      </Poster>
+
+
+
     </div>
   );
 }
